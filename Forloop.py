00ff84ba0db90357d41cf3c1642ab3e63197d7dd
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-03-29 09:55:11 +0530

    Add files via upload

---
+++ Forloop.py
@@ -0,0 +1,12 @@
+# i="helloo"
+# for a in i:
+#     print(a)
+# print("end")
+
+for i in range(1,-10,-1):
+    print(i)
+for j in range(0,10,2):
+    print(j)
+for k in range(-10,1):
+    print(k)
+
