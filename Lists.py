c433ec81a5066cbc45e50552aa5fe6ad2c1a0db0
Kishan Singh <136336528+devkishan03@users.noreply.github.com>
2024-05-02 23:40:01 +0530

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,13 @@
+mylist=['hi','hello','bye','send','recive',12,12.5]
+for x in mylist:
+    print(x,end=' ')
+print(mylist)
+print(mylist[2])
+mylist2=list((1,3,4,5,6,7,8,9))
+print(mylist2)
+for x in mylist2:
+    print(x)
+mylist2.append(11)
+print(mylist2)
+mylist2.insert(3)
+print(mylist2)
